962a45c416e793b1d6554fcd0e8384b0389a9366
Mert Kalkancı <63818504+MertKalkanci@users.noreply.github.com>
2023-02-17 16:00:28 +0300

    Basic Scene Setup

---
+++ Assets/Scripts/BasicAgent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class BasicAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+}
